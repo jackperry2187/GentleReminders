ac80b12bd235d8673989d44f74b997ba8739e706
Jackson Perry <jackperry2187@gmail.com>
2024-10-29 21:12:43 -0400

    - Rebrand from MindfulCraft to Gentle Reminders in 1.21.1 - Split into 1.21.1 and 1.20.1 versions of the mod

---
+++ gentle-reminders-1.21.1/src/client/java/jackperry2187/gentlereminders/config/ConfigSettings.java
@@ -0,0 +1,121 @@
+package jackperry2187.gentlereminders.config;
+
+import jackperry2187.gentlereminders.GentleReminders;
+import jackperry2187.gentlereminders.util.Message;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+@Environment(value = EnvType.CLIENT)
+public class ConfigSettings {
+    private static boolean isInitialized = false;
+
+    public static final Path configPath = FabricLoader.getInstance().getConfigDir();
+    public static final Path modConfigFile = configPath.resolve(GentleReminders.MOD_ID + "-config.toml");
+
+    public static int configVersion;
+    // TODO: private static boolean skipConfigVersionCheck;
+
+    public static boolean enabled;
+    public static int ticksBetweenMessages;
+
+    public static List<Message> messages = new ArrayList<>();
+    public static int highestMessageID = 0;
+
+    private static final boolean isDebug = false;
+    private static final int debugTicksBetweenMessages = 20 * 7; // 7 seconds
+
+    public static void initialize() {
+        if(isInitialized) {
+            GentleReminders.LOGGER.error("ConfigSettings have already been initialized!");
+            return;
+        }
+
+        if(!Files.exists(modConfigFile)) {
+            GentleReminders.LOGGER.error("Config file does not exist!");
+            throw new RuntimeException("Config file does not exist!");
+        }
+
+        readConfigFile();
+        if(isDebug) ticksBetweenMessages = debugTicksBetweenMessages;
+
+        messages.sort(Comparator.comparingInt(m -> m.ID));
+        highestMessageID = messages.get(messages.size() - 1).ID;
+
+        isInitialized = true;
+        if(isDebug) logConfigSettings();
+    }
+
+    private static void logConfigSettings() {
+        GentleReminders.LOGGER.info("Config Version: {}", configVersion);
+        // TODO: GentleReminders.LOGGER.info("Skip Config Version Check: {}", skipConfigVersionCheck);
+        GentleReminders.LOGGER.info("Enabled: {}", enabled);
+        GentleReminders.LOGGER.info("Time Between Messages: {} ticks, {} minutes", ticksBetweenMessages, ticksBetweenMessages / 20 / 60);
+//        GentleReminders.LOGGER.info("Messages:");
+//        for(Message message : messages) {
+//            GentleReminders.LOGGER.info("ID: {}", message.ID);
+//            GentleReminders.LOGGER.info("Title: {}", message.Title.getString());
+//            GentleReminders.LOGGER.info("Message: {}", message.Description.getString());
+//            GentleReminders.LOGGER.info("Enabled: {}", message.Enabled);
+//        }
+    }
+
+    private static void readConfigFile() {
+        try {
+            boolean isReadingMessages = false;
+            for(String line : Files.readAllLines(modConfigFile)) {
+                if (line.startsWith("#")) continue; // skip comments
+                if (line.startsWith("configVersion=")) {
+                    configVersion = Integer.parseInt(line.split("=")[1]);
+                }
+                // TODO: else if (line.startsWith("skipConfigVersionCheck=")) { skipConfigVersionCheck = Boolean.parseBoolean(line.split("=")[1]); }
+                else if(line.startsWith("enabled=")) {
+                    enabled = Boolean.parseBoolean(line.split("=")[1]);
+                } else if(line.startsWith("timeBetweenMessages=")) {
+                    ticksBetweenMessages = Integer.parseInt(line.split("=")[1]) * 20 * 60; // convert minutes to ticks
+                } else if(line.startsWith("messages=[")) {
+                    isReadingMessages = true;
+                } else if (isReadingMessages) {
+                    if(line.startsWith("]")) {
+                        isReadingMessages = false;
+                    }
+                    else {
+                        messages.add(parseMessage(line));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            GentleReminders.LOGGER.error("Failed to read config file!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Message parseMessage(String line) {
+        // line will be in format of:
+        // {id=#, title="String which does not contain a comma", message="String which does not contain a comma", enabled=boolean, titleColor="String", messageColor="String"},
+        // parse the line and create a new Message object
+        String[][] parts = Arrays.stream(line.split(",")).map(s -> s.trim().replace("\"", "").split("=")).toArray(String[][]::new);
+        int id = Integer.parseInt(parts[0][1]);
+        String title = parts[1][1];
+        String message = parts[2][1];
+        boolean enabled = Boolean.parseBoolean(parts[3][1]);
+        String titleColor = parts[4][1];
+        String messageColor = parts[5][1];
+
+        Text titleText = MutableText.of(Text.of(title).getContent()).formatted(Formatting.byName(titleColor));
+        Text messageText = MutableText.of(Text.of(message).getContent()).formatted(Formatting.byName(messageColor));
+
+        // create a new Message object and add it to the messages list
+        return new Message(id, titleText, messageText, enabled);
+    }
+}
---
+++ gentle-reminders-1.21.1/src/client/java/jackperry2187/gentlereminders/util/SendClientMessage.java
@@ -0,0 +1,73 @@
+package jackperry2187.gentlereminders.util;
+
+import jackperry2187.gentlereminders.GentleReminders;
+import jackperry2187.gentlereminders.config.ConfigSettings;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.toast.SystemToast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Environment(value = EnvType.CLIENT)
+public class SendClientMessage {
+    private static int lastMessageIndex = -1;
+    private static int numberOfMessagesSent = 0;
+    private static final List<Integer> idsOfUnsentMessages = new ArrayList<>();
+
+    public static void sendInitialMessage(MinecraftClient client) {
+        if(lastMessageIndex != -1) {
+            GentleReminders.LOGGER.error("Initial message has already been sent!");
+            return;
+        }
+        Message intialMessage = ConfigSettings.messages.get(0);
+
+        client.getToastManager().add(SystemToast.create(
+                client,
+                SystemToast.Type.PERIODIC_NOTIFICATION,
+                intialMessage.Title,
+                intialMessage.Description
+        ));
+
+        lastMessageIndex = 0;
+    }
+
+    public static void sendPeriodicMessage(MinecraftClient client) {
+        if(lastMessageIndex == -1) {
+            GentleReminders.LOGGER.error("Initial message has not been sent!");
+            return;
+        }
+
+        if(numberOfMessagesSent == ConfigSettings.messages.size() - 1) {
+            numberOfMessagesSent = 0;
+            idsOfUnsentMessages.clear();
+        }
+
+        if(idsOfUnsentMessages.isEmpty()) resetIdsOfUnsentMessages();
+
+        // generate a random index to get the next message
+        int nextMessageIndex = (int) (Math.random() * (idsOfUnsentMessages.size() - 1));
+        Message nextMessage = ConfigSettings.messages.get(idsOfUnsentMessages.get(nextMessageIndex));
+
+        client.getToastManager().add(SystemToast.create(
+                client,
+                SystemToast.Type.PERIODIC_NOTIFICATION,
+                nextMessage.Title,
+                nextMessage.Description
+        ));
+
+        numberOfMessagesSent++;
+        idsOfUnsentMessages.remove(nextMessageIndex);
+        lastMessageIndex = nextMessageIndex;
+    }
+
+    private static void resetIdsOfUnsentMessages() {
+        if(!idsOfUnsentMessages.isEmpty()) idsOfUnsentMessages.clear();
+
+        for (Message message : ConfigSettings.messages) {
+            if(!message.Enabled) continue;
+            idsOfUnsentMessages.add(message.ID);
+        }
+    }
+}
